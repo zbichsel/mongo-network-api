35175af5263a200074b3e7e1d2d7aa2e7d1bdce3
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-23 20:40:21 -0500

    created Thought model

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+    const Thought = model('thought', thoughtSchema);
+
+    module.exports = Thought;
+
+    // !-- getter method to format the timestamp on query --!
