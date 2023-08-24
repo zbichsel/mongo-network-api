fcd0697a3eafda64b59a01898a7f42538bff771a
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-23 21:03:02 -0500

    created userController

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID'});
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID'});
+            }
+
+            await Thought.deleteMany({ _id: { $in: user.thoughts } });
+            res.json({ message: 'User and associated thoughts deleted!' });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+};
